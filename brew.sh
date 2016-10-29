3044991cd0aa886618b144ed0b58494e325f0d2b
Fabio Pellacini <fabio.pellacini@gmail.com>
2016-10-29 22:49:39 +0200

    added more packages and update versions

--- brew.sh
+++ brew.sh
@@ -4,15 +4,17 @@ brew upgrade
 
 brew tap homebrew/versions
 
-brew install autojump
 brew install git
+brew install autojump
 brew install python --without-sqlite
+brew install python3
+brew install qt5
 brew install bash-completion
 brew install clang-format
 brew install the_silver_searcher
 brew install bash # bash4
 brew install ninja
-brew install --without-shared-library glfw3
+brew install glfw3
 
 brew install https://raw.githubusercontent.com/ryanmjacobs/c/master/c.rb
 
